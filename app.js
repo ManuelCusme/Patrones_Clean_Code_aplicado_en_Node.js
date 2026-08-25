5bd56957668b65bd5bf77516eeb8706984b79526
Manuel Cusme <cusmeman06@gmail.com>
2026-08-24 20:53:28 -0500

    se agregan productos para verificar escabilidad

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const cliente = crearCliente("Juan Pérez", "juan@gmail.com", "Ambato");
 const productos = [
     crearProducto("Laptop", 850, 1),
     crearProducto("Mouse", 25, 2),
-    crearProducto("Teclado", 40, 1)
+    crearProducto("Teclado", 40, 1),
+    crearProducto("Monitor", 200, 1),     // Nuevo producto
+    crearProducto("Audífonos", 30, 2)     // Nuevo producto
 ];
 
 // Procesar pedido
